80488f80948439e7e9d47e63cc7ccdb8227bb1bc
danalee <eleeje97@gmail.com>
2023-04-10 17:58:01 +0900

    Test: 구글 페이지 접근 테스트

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
+import static io.restassured.RestAssured.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -16,7 +17,10 @@ public class RestAssuredTest {
     @Test
     void accessGoogle() {
         // TODO: 구글 페이지 요청 구현
-        ExtractableResponse<Response> response = null;
+        ExtractableResponse<Response> response =
+                given().log().all()
+                                .when().get("https://www.google.co.kr/")
+                                .then().log().status().extract();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
