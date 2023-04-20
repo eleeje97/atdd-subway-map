81f17773315984cf400f121add599e516b540c2a
danalee <eleeje97@gmail.com>
2023-04-20 10:48:03 +0900

    Feature(Line): Line 도메인 생성

---
+++ src/main/java/subway/domain/Line.java
@@ -0,0 +1,63 @@
+package subway.domain;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Line {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String color;
+
+    private Long upStationId;
+
+    private Long downStationId;
+
+    @OneToMany
+    @JoinColumn(name = "line_id")
+    private List<Station> stations;
+
+    private int distance;
+
+    public Line(String name, String color, Long upStationId, Long downStationId, int distance) {
+        this.name = name;
+        this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public List<Station> getStations() {
+        return stations;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
